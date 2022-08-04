8b33a0d67720867f053f56de254ff92e44a60c33
chigaevi <chigaev.i@gmail.com>
2022-08-04 20:26:38 +0700

    version to check

--- main.py
+++ main.py
@@ -4,9 +4,6 @@ def counter_line(text):
         counter += 1
     return counter
 
-with open('4.txt', 'w') as cleaner:
-    pass
-
 with open('1.txt',encoding='utf-8') as one_text:
     quan_line_one = counter_line(one_text)
 with open('2.txt', encoding='utf-8') as two_text:
@@ -19,9 +16,9 @@ one_text = open('1.txt',encoding='utf-8')
 two_text =  open('2.txt', encoding='utf-8')
 three_text = open('3.txt', encoding='utf-8')
 
-list_line = [quan_line_one, quan_line_two, quan_line_three,-100]
+list_line = [quan_line_one, quan_line_two, quan_line_three]
 
-with open('4.txt', 'a', encoding='utf-8') as final_text:
+with open('4.txt', 'w', encoding='utf-8') as final_text:
     for i_ in range(3):
         if quan_line_one == max(list_line):
             final_text.write(f'1.txt\n{quan_line_one}\n')
@@ -30,14 +27,14 @@ with open('4.txt', 'a', encoding='utf-8') as final_text:
             final_text.write('\n')
             list_line.remove(quan_line_one)
 
-        if quan_line_two == max(list_line):
+        elif quan_line_two == max(list_line):
             final_text.write(f'2.txt\n{quan_line_two}\n')
             for line in two_text:
                 final_text.write(line)
             final_text.write('\n')
             list_line.remove(quan_line_two)
 
-        if quan_line_three == max(list_line):
+        elif quan_line_three == max(list_line):
             final_text.write(f'3.txt\n{quan_line_three}\n')
             for line in three_text:
                 final_text.write(line)
